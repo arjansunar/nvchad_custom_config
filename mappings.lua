e5c21da1e8af92d3363da873d564ce05d59952bc
rjan <rjan@pop-os.localdomain>
2023-04-28 05:32:12 +0545

    add tmux navigator options and using prettier to format typescript react files

--- mappings.lua
+++ mappings.lua
@@ -1,11 +1,20 @@
 ---@type MappingsTable
 local M = {}
 
+local opts = {
+  nowait = true,
+}
+
 M.general = {
   n = {
     -- [";"] = { ":", "enter command mode", opts = { nowait = true } },
-    ["<C-q>"] = { ":q!<CR>", "force exit", opts = { nowait = true } },
-    ["<leader>;"] = { 'ciw`<c-r>"`', "Wrap with `", opts = { nowait = true } },
+    ["<C-q>"] = { ":q!<CR>", "force exit", opts },
+    ["<leader>;"] = { 'ciw`<c-r>"`', "Wrap with `", opts },
+
+    ["<C-h>"] = { "<cmd> NvimTmuxNavigateLeft<CR>", "window left" },
+    ["<C-l>"] = { "<cmd> NvimTmuxNavigateRight<CR>", "window right" },
+    ["<C-j>"] = { "<cmd> NvimTmuxNavigateDown<CR>", "window down" },
+    ["<C-k>"] = { "<cmd> NvimTmuxNavigateUp<CR>", "window up" },
   },
 }
 
